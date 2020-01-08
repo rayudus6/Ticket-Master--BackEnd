0f56711cd3f2f842ff3bcc45d19f6a7b2ec7bcff
Subba Rayudu <subba.rayudu@kloctechnologies.co.in>
2020-01-09 01:02:21 +0530

    routers created for employees

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const {Ticket}=require('./models/ticket');
 
 const { Employee }=require('./models/employee');
 
+const employeeRouter=require('./routers/employees');
+
 const port=3000;
 
 //middlewares
@@ -129,88 +131,8 @@ app.delete('/tickets/:id',(req,res)=>{
     })
 });
 
-app.get('/employees',(req,res) =>{
-    Employee.find().then((employees) =>{
-        res.send(employees);
-    })
-});
+app.use('/employees',employeeRouter);
 
-app.post('/employees',(req,res) =>{
-    let body=_.pick(req.body,['name','email','department','salary','ageWhileJoining','address','hobbies','luckyNumber','mobileNumbers'])
-    let employee=new Employee(body);
-    employee.save().then((employee) =>{
-        res.send(employee);
-    })
-})
-
-app.get('/employees/:id/mobile_numbers',(req,res) =>{
-    let id=req.params.id;
-    Employee.findById(id).select(['name','_id','mobileNumbers'])
-    .then((employee) =>{
-        res.send(employee);
-    })
-    .catch((err) =>{
-        res.send(err)
-    })
-})
-
-app.post('/employees/:id/mobile_numbers',(req,res) =>{
-    let id=req.params.id;
-    let body=req.body;
-    Employee.findById(id).then((employee)=>{
-        if(employee){
-            employee.mobileNumbers.push(body);
-            employee.save().then((employee)=>{
-                res.send(employee.mobileNumbers);
-            })
-        }else{
-            res.send('Invalid id');
-        }
-    })
-
-});
-
-app.delete('/employees/:id/mobile_numbers/:mobile_id',(req,res) =>{
-    let id=req.params.id;
-    let mobile_id=req.params.mobile_id;
-    Employee.findById(id).then((employee)=>{
-        if(employee){
-            employee.mobileNumbers.remove(mobile_id);
-            return employee.save();
-        }
-        res.send({
-            notice:'Invalid id'
-        })
-    }).then((employee)=>{
-        res.send({
-            Notice:'Successfully contact removed'
-        })
-    })
-});
-
-app.put('/employees/:id/mobile_numbers/:mobile_id',(req,res) =>
-{
-    let id=req.params.id;
-    let mobileId=req.params.mobile_id;
-    let body=_.pick(req.body,['numType','mobileNumber']);
-    Employee.findById(id).then((employee) =>{
-        if(employee){
-            let mobileDetail=employee.mobileNumbers.id(mobileId);
-            console.log(mobileDetail)
-            mobileDetail.numType=body.numType ? body.numType : mobileDetail.numType;
-            mobileDetail.mobileNumber=body.mobileNumber ? body.mobileNumber : mobileDetail.mobileNumber;
-            return employee.save();
-        }
-        res.send({
-            notice:'Invalid id'
-        })
-    }).then((employee) =>{
-        res.send({
-            mobileNumber: employee.mobileNumbers.id(mobileId),
-            notice:'Successfully Updated the number'
-        })
-    })
-})
 app.listen(port,() =>{
     console.log('Listening port is',port);
 });
---
+++ routers/employees.js
@@ -0,0 +1,93 @@
+const express=require('express');
+
+const { Employee } =require('../models/employee');
+
+const _=require('lodash');
+
+const router=express.Router();
+
+
+router.get('/',(req,res) =>{
+    Employee.find().then((employees) =>{
+        res.send(employees);
+    })
+});
+
+router.post('/',(req,res) =>{
+    let body=_.pick(req.body,['name','email','department','salary','ageWhileJoining','address','hobbies','luckyNumber','mobileNumbers'])
+    let employee=new Employee(body);
+    employee.save().then((employee) =>{
+        res.send(employee);
+    })
+})
+
+router.get('/:id/mobile_numbers',(req,res) =>{
+    let id=req.params.id;
+    Employee.findById(id).select(['name','_id','mobileNumbers'])
+    .then((employee) =>{
+        res.send(employee);
+    })
+    .catch((err) =>{
+        res.send(err)
+    })
+})
+
+router.post('/:id/mobile_numbers',(req,res) =>{
+    let id=req.params.id;
+    let body=req.body;
+    Employee.findById(id).then((employee)=>{
+        if(employee){
+            employee.mobileNumbers.push(body);
+            employee.save().then((employee)=>{
+                res.send(employee.mobileNumbers);
+            })
+        }else{
+            res.send('Invalid id');
+        }
+    })
+
+});
+
+router.delete('/:id/mobile_numbers/:mobile_id',(req,res) =>{
+    let id=req.params.id;
+    let mobile_id=req.params.mobile_id;
+    Employee.findById(id).then((employee)=>{
+        if(employee){
+            employee.mobileNumbers.remove(mobile_id);
+            return employee.save();
+        }
+        res.send({
+            notice:'Invalid id'
+        })
+    }).then((employee)=>{
+        res.send({
+            Notice:'Successfully contact removed'
+        })
+    })
+});
+
+router.put('/:id/mobile_numbers/:mobile_id',(req,res) =>
+{
+    let id=req.params.id;
+    let mobileId=req.params.mobile_id;
+    let body=_.pick(req.body,['numType','mobileNumber']);
+    Employee.findById(id).then((employee) =>{
+        if(employee){
+            let mobileDetail=employee.mobileNumbers.id(mobileId);
+            console.log(mobileDetail)
+            mobileDetail.numType=body.numType ? body.numType : mobileDetail.numType;
+            mobileDetail.mobileNumber=body.mobileNumber ? body.mobileNumber : mobileDetail.mobileNumber;
+            return employee.save();
+        }
+        res.send({
+            notice:'Invalid id'
+        })
+    }).then((employee) =>{
+        res.send({
+            mobileNumber: employee.mobileNumbers.id(mobileId),
+            notice:'Successfully Updated the number'
+        })
+    })
+});
+
+module.exports=router;
